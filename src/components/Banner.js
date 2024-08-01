62e5b58a736c3e3386c428ca5a0312804ad7a9bf
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 17:27:50 +0530

    commit 26

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className='h-[30vh] md:h-[80vh] bg-center bg-no-repeat flex items-end'
+    <div className='h-[20vh] md:h-[60vh] bg-center bg-no-repeat flex items-end'
         style = {{
             backgroundImage :  `url(https://i.pinimg.com/564x/ee/a8/fa/eea8fa698bc52a96791f637c4de3ac3e.jpg)`
         }}>
