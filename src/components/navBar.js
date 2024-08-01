206357dbc3ccd888acdb0dee2660de914990537b
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 16:29:42 +0530

    commit 15

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import movieLogo from '../movieLogo.png'
+import movieLogo from './movieLogo.png'
 function navBar() {
   return (    
     <div className = 'flex border space-x-8 items-center pl-3 py-4'>  
