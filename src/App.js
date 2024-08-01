fd224b00448e64e0568104450e020802dd6605a8
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 15:50:49 +0530

    commit 7

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import navBar from './components/navBar'
 function App() {
   return (
     <div className = 'flex border space-x-8 items-center pl-3 py-4'>       
-    <h3>Movies</h3>
-    <h3>WatchList</h3>
+    <h3 className = 'text-blue-400'>Movies</h3>
+    <h3 className = 'text-blue-400'>WatchList</h3>
     </div>
   );
 }
