5cc629180221cabdcd6739697cb2dabe3a4013a4
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 15:23:41 +0530

    commit 4

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">      
-          <h1 classname = 'text-xl text-blue-400'>React App IMDB</h1>        
+          <h1 classname = 'text-blue-400'>React App IMDB</h1>        
     </div>
   );
 }
