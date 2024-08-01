f27c956c544272ef7b47829e8fb918082ceb0b01
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 17:41:20 +0530

    commit 31

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ function Banner() {
         style = {{
             backgroundImage :  `url(https://i.pinimg.com/564x/ee/a8/fa/eea8fa698bc52a96791f637c4de3ac3e.jpg)`
         }}>
-            <div className='text-xl md:text-3xl bg-gray-900 bg-opacity-60 p-4 text-white text-center w-full'>Satakarni</div>
+            <div className='text-xl md:text-3xl bg-gray-90 bg-opacity-60 p-4 text-white text-center w-full'>Satakarni</div>
     </div>
   )
 }
