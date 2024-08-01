a820ec3237518b8e87db306d6542042a0ea313fb
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 16:20:08 +0530

    commit 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
 import navBar from './components/navBar'
-import movieLogo from './components/movieLogo.png'
+import Banner from './components/Banner'
 function App() {
   return (
-    <div className = 'flex border space-x-8 items-center pl-3 py-4'>  
-    <img src={movieLogo} alt="" className = 'w-[50px]'/>     
-    <h3 className = 'text-blue-400'>Movies</h3>
-    <h3 className = 'text-blue-400'>WatchList</h3>
+    <div>
+      <navBar/>
+      <Banner/>
     </div>
   );
 }
