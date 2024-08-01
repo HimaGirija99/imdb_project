842132838413144b04338ff464b0f94a435341f0
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 16:39:33 +0530

    commit 19

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Logo from '../movieLogo.png'
+import movieLogo from '../movieLogo.png'
 function navBar() {
   return (    
     <div className = 'flex border space-x-8 items-center pl-3 py-4'>  
-        <img src={Logo} alt="" className = 'w-[50px]'/>     
+        <img src={movieLogo} alt="" className = 'w-[50px]'/>     
         <h3 className = 'text-blue-400'>Movies</h3>
         <h3 className = 'text-blue-400'>WatchList</h3>
     </div>      
