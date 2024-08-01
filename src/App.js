ef4ad0db28d66516a715e6068749a4814e61f093
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-01 16:09:43 +0530

    commit 8

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import movieLogo from './components/movieLogo.png'
 function App() {
   return (
     <div className = 'flex border space-x-8 items-center pl-3 py-4'>  
-    <img src={movieLogo} className = 'w-[50px]'/>     
+    <img src={movieLogo} alt="" className = 'w-[50px]'/>     
     <h3 className = 'text-blue-400'>Movies</h3>
     <h3 className = 'text-blue-400'>WatchList</h3>
     </div>
