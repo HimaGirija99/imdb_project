7a938766340209820f5b22e54a1fb1f8362a5779
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-02 14:56:04 +0530

    commit-26

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -20,7 +20,7 @@ function Movies() {
     <div className='text-2xl mb-8 font-bold text-center'>Trending Movies</div>    
     <div className='flex flex-wrap'>
       {movies.map((movie)=>{
-        return <div className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 md:h-[40vh] md:w-[180px] hover:scale-110 duration-300 relative flex items-end' style= {{backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`}}
+        return <div key={movie.id} className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 md:h-[40vh] md:w-[180px] hover:scale-110 duration-300 relative flex items-end' style= {{backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`}}
         ><div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-60">{movie.title}</div>
         </div>
       })}
