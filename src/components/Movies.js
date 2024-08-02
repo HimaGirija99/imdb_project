de5c175c3355fc28e4a13d62b4f7907a2a39de92
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-02 11:39:40 +0530

    commit-4

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Movies() {
   return (
-    <div className="text-2xl mb-8 font-bold text-center">Trending Movies</div>
+    <div>
+    <div className='text-2xl mb-8 font-bold text-center'>Trending Movies</div>
+    
+    
+    <div className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl' style= {{backgroundImage: `url(https://i.pinimg.com/564x/ee/a8/fa/eea8fa698bc52a96791f637c4de3ac3e.jpg)`}}></div>
+    </div>
   )
 }
 
