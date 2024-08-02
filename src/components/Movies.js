af61d0e7daee16c37cbaee8b70021ff0fcff6175
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-02 13:05:48 +0530

    commit-17

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,12 +4,12 @@ function Movies() {
   return (
     <div>
     <div className='text-2xl mb-8 font-bold text-center'>Trending Movies</div>    
-    <div className='flex'>
+    
     <div className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 md:h-[40vh] md:w-[180px] hover:scale-110 duration-300 relative flex items-end' style= {{backgroundImage: `url(https://i.pinimg.com/564x/ee/a8/fa/eea8fa698bc52a96791f637c4de3ac3e.jpg)`}}></div>
     <div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-60">
                 Satakarni
               </div>
-    </div>
+    
     </div>
   )
 }
