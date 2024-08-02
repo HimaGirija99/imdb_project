edb49077f801a7fc4a5a110a36d364cfde227859
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-02 17:29:31 +0530

    commit-33

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 import Pagination from './Pagination'
+
 function Movies() {
   const[movies, setMovies] = useState([])
   const[pageNum, setPageNum] = useState(1)
+  const [watchList, setWatchList] = useState([]);
   const onNext=()=>{
       setPageNum(pageNum+1)
   }
@@ -12,6 +14,17 @@ function Movies() {
       setPageNum(pageNum-1)
     }
   }
+  const addToWatchList = (movie) => {
+    const newWatchList = [...watchList, movie];
+    setWatchList(newWatchList);
+  };
+  const removeFromWatchList = (movie) => {
+    const filteredWatchList = watchList.filter((m) => {
+      return (m.id!==movie.id);
+    });
+
+    setWatchList(filteredWatchList);    
+  };
   useEffect(()=>{
     //IIFE
     (function(){
@@ -29,7 +42,14 @@ function Movies() {
     <div className='flex flex-wrap'>
       {movies.map((movie)=>{
         return <div key={movie.id} className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 md:h-[40vh] md:w-[180px] hover:scale-110 duration-300 relative flex items-end' style= {{backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie.poster_path})`,}}
-        ><div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-60">{movie.title}</div>
+        ><div className="text-2xl p-2 bg-gray-900 rounded-2xl absolute right-2 top-2">
+         {watchList.includes(movie) === false ? (
+           <div onClick={() => addToWatchList(movie)}> 😍 </div>
+         ) : (
+           <div onClick={() => removeFromWatchList(movie)}>❌</div>
+         )}
+       </div>
+          <div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-60">{movie.title}</div>
         </div>
       })}
     
