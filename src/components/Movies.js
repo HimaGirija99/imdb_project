95d728374a0d77950b364e963045bbcad322a2f3
HimaGirija Ponnapalli <girija.bio@gmail.com>
2024-08-02 11:43:53 +0530

    commit-5

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,7 +6,7 @@ function Movies() {
     <div className='text-2xl mb-8 font-bold text-center'>Trending Movies</div>
     
     
-    <div className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl' style= {{backgroundImage: `url(https://i.pinimg.com/564x/ee/a8/fa/eea8fa698bc52a96791f637c4de3ac3e.jpg)`}}></div>
+    <div className='w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 mh-[40vh] md-[180px]' style= {{backgroundImage: `url(https://i.pinimg.com/564x/ee/a8/fa/eea8fa698bc52a96791f637c4de3ac3e.jpg)`}}></div>
     </div>
   )
 }
